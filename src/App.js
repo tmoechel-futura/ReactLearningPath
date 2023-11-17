bd6fb2a60ee82d519fec9d7b460e2997cf62514b
tmoechel-futura <t.moechel@futura-solutions.de>
2023-11-17 22:33:42 +0100

    useState Hook examples

--- src/App.js
+++ src/App.js
@@ -1,52 +1,51 @@
 import styles from "./App.module.css";
-import UserInfo from "./Components/UserInfo";
-import UserList from "./Components/UserList";
-import Job from "./Components/Job";
-import Planet from "./Components/Planet";
+import { useState } from "react";
 
 function App() {
-  const isGreen = false;
-  const names = ["Mike", "Peter", "Bob"];
+  const [age, setAge] = useState(1);
+  const [inputValue, setInputValue] = useState("initial message");
+  const [hideText, setHideText] = useState(false);
+  const [textColor, setTextColor] = useState("green");
 
-  const users = [
-    { name: "Bob", age: 33 },
-    { name: "Mike", age: 44 },
-  ];
+  const increaseAge = () => {
+    setAge(age + 1);
+  };
 
-  const planets = [
-    { name: "Mars", isGasPlanet: false },
-    { name: "Earth", isGasPlanet: false },
-    { name: "Jupiter", isGasPlanet: true },
-    { name: "Venus", isGasPlanet: false },
-  ];
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleHideButtonClick = () => {
+    setHideText(!hideText);
+    setTextColor(textColor === "green" ? "red": "green");
+  };
+
+  const handleIncreaseButtonClick = () => {
+    setAge(age + 1);
+  }
+
+  const handleDecreaseButtonClick = () => {
+    setAge(age -1);
+  }
+
+  const handleSetToZeroClick = () => {
+    setAge(0);
+  }
 
   return (
     <div className={styles.App}>
-      <UserInfo firstName="Thomas" lastName="Möchel" age={61} />
-      <UserInfo firstName="Markus" lastName="Möchel" age={57} />
-      <UserInfo firstName="Ursel" lastName="Möchel" age={60} />
-      <Job position="Software Architect" salary={70000} />
-      <Job position="Software Developer" salary={60000} />
-      <h1 style={{ color: isGreen ? "green" : "red" }}> This has a color </h1>
-      {/* other example of a logical operator (acts like if ) */}
-      <h1> {isGreen && <button>this is a button</button>}</h1>
-      {names.map((name, key) => {
-        return <h1 key={key}> {name}</h1>;
-      })}
-      <h3> User Component test </h3>
-
-      <UserList userList={users} />
-
-      <h3> Show only planets that are not Gasplanets </h3>
-      {planets.map((planet, key) => {
-        return (
-          !planet.isGasPlanet && (
-            <div key={key}>
-              <Planet planet={planet} />
-            </div>
-          )
-        );
-      })}
+      <h2> {age}</h2>
+      <h2 style={{ color: textColor }}> {inputValue}</h2>
+      <button onClick={increaseAge}> Increase Age </button>
+      {hideText && (
+        <input onChange={handleInputChange} value={inputValue} type="text" />
+      )}
+      <p />
+      <button onClick={handleHideButtonClick}> Toggle input </button>
+      <p />
+      <button onClick={handleIncreaseButtonClick}> Increase </button>
+      <button onClick={handleDecreaseButtonClick}> Decrease </button>
+      <button onClick={handleSetToZeroClick}> Set to 0 </button>
     </div>
   );
 }
--- src/Components/Planet.js
+++
@@ -1,5 +0,0 @@
-const Planet = (props) => {
-  return <h2> {props.planet.name} </h2>;
-};
-
-export default Planet;
